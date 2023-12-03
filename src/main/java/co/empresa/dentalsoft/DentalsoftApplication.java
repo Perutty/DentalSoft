a829f7fd45a3b4472e136f3b2a87e1387b7cc304
brayanperutty <brayanperutty@gmail.com>
2023-12-02 19:55:59 -0500

    "Se ajustan los parámetros del properties para la conexión con la base de datos"

--- src/main/java/co/empresa/dentalsoft/DentalsoftApplication.java
+++ src/main/java/co/empresa/dentalsoft/DentalsoftApplication.java
@@ -1,19 +1,12 @@
 package co.empresa.dentalsoft;
 
-
-import java.io.File;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import co.empresa.dentalsoft.controller.AdministradorController;
-
 
 @SpringBootApplication
 public class DentalsoftApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(DentalsoftApplication.class, args);
 	}
 
